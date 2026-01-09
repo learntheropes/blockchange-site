b38603bc7975e1417c11f5095e19848e6800bb1e
Learntheropes <giovanni@learntheropes.xyz>
2026-01-09 13:54:44 +0100

    remove sitemap.xml from repo

--- plugins/cal-forward-query.client.js
+++ plugins/cal-forward-query.client.js
@@ -1,5 +1,4 @@
 export default defineNuxtPlugin(() => {
-  // Must run before the embed init, but in practice it's OK as long as it's set early on client
   if (typeof window === 'undefined') return
 
   window.Cal = window.Cal || {}
